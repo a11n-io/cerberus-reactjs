7f4ef1ef424c471e7885d841e8cc63876b1f3477
superkruger <superkruger@gmail.com>
2022-11-21 12:43:51 +0100

    add hook for access checking

---
+++ src/components/useAccess/useAccess.js
@@ -0,0 +1,37 @@
+import { useContext, useEffect, useState } from 'react'
+import { CerberusContext } from '../CerberusContext'
+import { ReadyState } from 'react-use-websocket'
+
+const useAccess = ({ resourceId, action, setGranted }) => {
+  const [messageId, setMessageId] = useState('')
+  const cerberusCtx = useContext(CerberusContext)
+
+  useEffect(() => {
+    if (cerberusCtx.readyState === ReadyState.OPEN) {
+      // eslint-disable-next-line no-undef
+      const msgId = crypto.randomUUID()
+      setMessageId(msgId)
+
+      cerberusCtx.sendMessage(
+        JSON.stringify({
+          messageId: msgId,
+          hasAccessRequest: {
+            resourceId: resourceId,
+            actionName: action
+          }
+        })
+      )
+    }
+  }, [cerberusCtx.sendMessage, cerberusCtx.readyState])
+
+  useEffect(() => {
+    if (cerberusCtx.lastMessage && cerberusCtx.lastMessage.data) {
+      const msg = JSON.parse(cerberusCtx.lastMessage.data)
+      if (msg && msg.messageId === messageId) {
+        setGranted(msg.granted)
+      }
+    }
+  }, [cerberusCtx.lastMessage])
+}
+
+export default useAccess
