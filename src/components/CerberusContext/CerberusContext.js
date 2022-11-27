fc1b265792a3673f5dbff7bb5daf6193ded58409
superkruger <superkruger@gmail.com>
2022-11-27 12:46:06 +0100

    store token in local storage

--- src/components/CerberusContext/CerberusContext.js
+++ src/components/CerberusContext/CerberusContext.js
@@ -1,11 +1,17 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext } from 'react'
 import useWebSocket from 'react-use-websocket'
+import useLocalStorageState from 'use-local-storage-state'
 
 const CerberusContext = createContext(null)
 
 function CerberusProvider(props) {
   const { cerberusUrl, socketUrl, suffix = '' } = props
-  const [cerberusToken, setCerberusToken] = useState()
+  const [cerberusToken, setCerberusToken] = useLocalStorageState(
+    `a11n-cerberus-api-token`,
+    {
+      defaultValue: null
+    }
+  )
 
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
     share: true,
